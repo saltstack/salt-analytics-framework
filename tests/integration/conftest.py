700aef89d829ccd6c7e5c7dad9adc0473a67c7a1
Pedro Algarvio <palgarvio@vmware.com>
2021-11-12 06:49:52 +0000

    Adjust fixtures
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -6,11 +6,9 @@ import shutil
 from typing import Iterator
 
 import pytest
-from saltfactories.cli.call import SaltCall
 from saltfactories.cli.run import SaltRun
 from saltfactories.cli.salt import SaltCli
 from saltfactories.daemons.master import SaltMaster
-from saltfactories.daemons.minion import SaltMinion
 from saltfactories.utils import random_string
 
 
@@ -23,47 +21,20 @@ def analytics_events_dump_directory(tmp_path_factory) -> Iterator[pathlib.Path]:
         shutil.rmtree(str(dump_path), ignore_errors=True)
 
 
+@pytest.fixture(autouse=True)
+def cleanup_analytics_events_dump_directory(
+    analytics_events_dump_directory: pathlib.Path,
+) -> Iterator[pathlib.Path]:
+    try:
+        yield analytics_events_dump_directory
+    finally:
+        for path in analytics_events_dump_directory.iterdir():
+            path.unlink()
+
+
 @pytest.fixture(scope="package")
 def master(salt_factories, analytics_events_dump_directory) -> SaltMaster:
-    default_config = {
-        "engines": ["analytics"],
-    }
-    factory = salt_factories.salt_master_daemon(random_string("master-"), defaults=default_config)
-    analytics_config = """
-    collectors:
-      noop-collector:
-        plugin: noop
-        interval: 1
-
-    processors:
-      noop-processor:
-        plugin: noop
-
-
-    forwarders:
-      disk-forwarder:
-        plugin: disk
-        path: {}
-
-
-    pipelines:
-      my-pipeline:
-        collect: noop-collector
-        process: noop-processor
-        forward: disk-forwarder
-    """.format(
-        analytics_events_dump_directory
-    )
-    with pytest.helpers.temp_file(
-        "analytics", contents=analytics_config, directory=factory.config_dir
-    ):
-        with factory.started():
-            yield factory
-
-
-@pytest.fixture(scope="package")
-def minion(master: SaltMaster) -> SaltMinion:
-    factory = master.salt_minion_daemon(random_string("minion-"))
+    factory = salt_factories.salt_master_daemon(random_string("master-"))
     with factory.started():
         yield factory
 
@@ -76,8 +47,3 @@ def salt_run_cli(master: SaltMaster) -> SaltRun:
 @pytest.fixture
 def salt_cli(master: SaltMaster) -> SaltCli:
     return master.get_salt_cli()
-
-
-@pytest.fixture
-def salt_call_cli(minion: SaltMinion) -> SaltCall:
-    return minion.get_salt_call_cli()
--- tests/integration/engines/test_engine.py
+++ tests/integration/engines/test_engine.py
@@ -4,9 +4,49 @@
 import time
 
 import pytest
+from saltfactories.daemons.master import SaltMaster
+from saltfactories.daemons.minion import SaltMinion
+from saltfactories.utils import random_string
 
 
-@pytest.mark.usefixtures("master")
+@pytest.fixture(scope="module", autouse=True)
+def minion(master: SaltMaster, analytics_events_dump_directory) -> SaltMinion:
+    default_config = {
+        "engines": ["analytics"],
+    }
+    factory = master.salt_minion_daemon(random_string("minion-"), defaults=default_config)
+    analytics_config = """
+    collectors:
+      noop-collector:
+        plugin: noop
+        interval: 1
+
+    processors:
+      noop-processor:
+        plugin: noop
+
+
+    forwarders:
+      disk-forwarder:
+        plugin: disk
+        path: {}
+
+
+    pipelines:
+      my-pipeline:
+        collect: noop-collector
+        process: noop-processor
+        forward: disk-forwarder
+    """.format(
+        analytics_events_dump_directory
+    )
+    with pytest.helpers.temp_file(
+        "analytics", contents=analytics_config, directory=factory.config_dir
+    ):
+        with factory.started():
+            yield factory
+
+
 def test_events_dumped_to_disk(analytics_events_dump_directory):
     timeout = 10
     while timeout:
