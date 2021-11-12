e3159b9aba6987e380817c5f56d783230c623ede
Pedro Algarvio <palgarvio@vmware.com>
2021-11-12 07:07:05 +0000

    Restart pipelines which fail while running.
    Refs #1
    
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- src/saf/pipeline.py
+++ src/saf/pipeline.py
@@ -36,32 +36,51 @@ class Pipeline:
         Run the pipeline.
         """
         log.info("Pipeline %r started", self.name)
+        while True:
+            try:
+                await self._run()
+            except asyncio.CancelledError:
+                log.info("Pipeline %r canceled", self.name)
+                break
+            except Exception as exc:
+                log.error(
+                    "Restarting pipeline %s due to an error: %s", self.name, exc, exc_info=True
+                )
+
+    async def _run(self) -> None:
         collect_plugin = self.collect_config.loaded_plugin
-        try:
-            async for event in collect_plugin.collect(config=self.collect_config):  # type: ignore[attr-defined]
-                # Process the event
-                for process_config in self.process_configs:
-                    # We pass copies of the event so that, in case an exception occurs while
-                    # the event is being processed, and the event has already been modified,
-                    # the next processor to run will get an unmodified copy of the event, not
-                    # the partially processed event
-                    original_event = event.copy()
-                    process_plugin = process_config.loaded_plugin
-                    try:
-                        event = await process_plugin.process(  # type: ignore[attr-defined]
-                            config=process_config,
-                            event=event,
-                        )
-                    except Exception as exc:  # pylint: disable=broad-except
-                        log.error("An exception occurred while processing the event: %s", exc)
-                        # Restore the original event
-                        event = original_event
-                # Forward the event
-                for forward_config in self.forward_configs:
-                    forward_plugin = forward_config.loaded_plugin
+        async for event in collect_plugin.collect(config=self.collect_config):  # type: ignore[attr-defined]
+            # Process the event
+            for process_config in self.process_configs:
+                # We pass copies of the event so that, in case an exception occurs while
+                # the event is being processed, and the event has already been modified,
+                # the next processor to run will get an unmodified copy of the event, not
+                # the partially processed event
+                original_event = event.copy()
+                process_plugin = process_config.loaded_plugin
+                try:
+                    event = await process_plugin.process(  # type: ignore[attr-defined]
+                        config=process_config,
+                        event=event,
+                    )
+                except Exception as exc:  # pylint: disable=broad-except
+                    log.error(
+                        "An exception occurred while processing the event: %s", exc, exc_info=True
+                    )
+                    # Restore the original event
+                    event = original_event
+            # Forward the event
+            for forward_config in self.forward_configs:
+                forward_plugin = forward_config.loaded_plugin
+                try:
                     await forward_plugin.forward(  # type: ignore[attr-defined]
                         config=forward_config,
                         event=event.copy(),
                     )
-        except asyncio.CancelledError:
-            log.info("Pipeline %r canceled", self.name)
+                except Exception as exc:  # pylint: disable=broad-except
+                    log.error(
+                        "An exception occurred while forwarding the event through config %r: %s",
+                        forward_config,
+                        exc,
+                        exc_info=True,
+                    )
