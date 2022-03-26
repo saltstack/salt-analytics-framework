8a88806d3c5e3eafdc38bbd24f6884e319b24226
Pedro Algarvio <palgarvio@vmware.com>
2022-03-26 11:06:46 +0000

    Run asyncio fixtures in strict mode. Fix method name and await canceled task.
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- tests/functional/manager/conftest.py
+++ tests/functional/manager/conftest.py
@@ -1,7 +1,22 @@
 # Copyright 2021-2022 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
 #
+import asyncio
+
 import pytest
+import pytest_asyncio
+
+from saf.manager import Manager
+from saf.models import AnalyticsConfig
+
+
+async def _run_manager(manager):
+    try:
+        await manager.run()
+    except asyncio.CancelledError:
+        pass
+    finally:
+        await manager.await_stopped()
 
 
 @pytest.fixture
@@ -30,3 +45,21 @@ def analytics_config_dict():
         },
         "salt_config": {},
     }
+
+
+@pytest.fixture
+def analytics_config(analytics_config_dict):
+    return AnalyticsConfig.parse_obj(analytics_config_dict)
+
+
+@pytest_asyncio.fixture
+async def manager(analytics_config):
+    _manager = Manager(analytics_config)
+    loop = asyncio.get_event_loop()
+    task = loop.create_task(_run_manager(_manager))
+    try:
+        yield _manager
+    finally:
+        if not task.done():
+            task.cancel()
+        await task
