2fdf253b46620e620330f6efb042989103cbe8f5
Pedro Algarvio <palgarvio@vmware.com>
2022-07-05 08:27:53 +0100

    Don't capture stderr when getting the version
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- noxfile.py
+++ noxfile.py
@@ -574,6 +574,7 @@ def changelog(session, draft):
         "--version",
         silent=True,
         log=False,
+        stderr=None,
     ).strip()
 
     town_cmd = ["towncrier", "build", f"--version={version}"]
