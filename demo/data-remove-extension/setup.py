5e9290b2ac2d1281f65744bd7f3dab935449026e
MKLeb <calebb@vmware.com>
2023-04-28 13:26:34 -0400

    remove the demo folder

--- demo/data-remove-extension/setup.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2022 VMware, Inc.
-# SPDX-License-Identifier: Apache-2.0
-#
-# pylint: disable=missing-module-docstring
-import setuptools
-
-if __name__ == "__main__":
-    setuptools.setup()
