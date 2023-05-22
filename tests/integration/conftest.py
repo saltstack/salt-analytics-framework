413e43a1b379f34bb6581f862792c297cccf8a81
Pedro Algarvio <palgarvio@vmware.com>
2023-05-22 18:00:32 +0100

    Simplify the integration tests config definition and execution
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -3,47 +3,83 @@
 #
 from __future__ import annotations
 
-import asyncio
-from typing import Any
-from typing import AsyncIterator
-
 import pytest
-import pytest_asyncio
 
-from saf.manager import Manager
 from saf.models import AnalyticsConfig
-
-try:
-    asyncio_fixture = pytest_asyncio.fixture
-except AttributeError:
-    # On Py3.6 and older version of the pytest gets installed which does not
-    # have the `.fixture` function.
-    # Fallback to `pytest.fixture`.
-    asyncio_fixture = pytest.fixture
-
-
-async def _run_manager(manager):
-    try:
-        await manager.run()
-    except asyncio.CancelledError:
-        pass
-    finally:
-        await manager.await_stopped()
+from saf.pipeline import Pipeline
 
 
 @pytest.fixture
-def analytics_config(analytics_config_dict: dict[str, Any]):
-    return AnalyticsConfig.parse_obj(analytics_config_dict)
+def collectors_config():
+    return {
+        "test-collector": {
+            "plugin": "test",
+            "count": 3,
+            "interval": 0.05,
+        },
+    }
 
 
-@asyncio_fixture
-async def manager(analytics_config: AnalyticsConfig) -> AsyncIterator[Manager]:
-    _manager = Manager(analytics_config)
-    loop = asyncio.get_event_loop()
-    task = loop.create_task(_run_manager(_manager))
-    try:
-        yield _manager
-    finally:
-        if not task.done():
-            task.cancel()
-        await task
+@pytest.fixture
+def processors_config():
+    return {
+        "test-processor": {
+            "plugin": "test",
+        },
+    }
+
+
+@pytest.fixture
+def forwarded_events_path(tmp_path):
+    return tmp_path / "forwarded-events"
+
+
+@pytest.fixture
+def forwarders_config(forwarded_events_path):
+    return {
+        "test-forwarder": {
+            "plugin": "disk",
+            "path": forwarded_events_path,
+        },
+    }
+
+
+@pytest.fixture
+def pipeline_name():
+    return "default-pipeline"
+
+
+@pytest.fixture
+def pipelines_config(collectors_config, processors_config, forwarders_config, pipeline_name):
+    collectors = list(collectors_config)
+    if len(collectors) > 1:
+        pytest.fail("Only one collector can be defined")
+    return {
+        pipeline_name: {
+            "collect": collectors[0],
+            "process": list(processors_config),
+            "forward": list(forwarders_config),
+            "enabled": True,
+            "restart": False,
+        },
+    }
+
+
+@pytest.fixture
+def analytics_config(
+    collectors_config, processors_config, forwarders_config, pipelines_config, pipeline_name
+):
+    return AnalyticsConfig.parse_obj(
+        {
+            "collectors": collectors_config,
+            "processors": processors_config,
+            "forwarders": forwarders_config,
+            "pipelines": pipelines_config,
+            "salt_config": {},
+        }
+    )
+
+
+@pytest.fixture
+def pipeline(analytics_config, pipeline_name):
+    return Pipeline(pipeline_name, analytics_config.pipelines[pipeline_name])
