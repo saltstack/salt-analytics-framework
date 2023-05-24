df361ea97acd932a35fbec2a93fc5f9237561923
MKLeb <calebb@vmware.com>
2023-05-24 18:01:02 -0400

    Initial stripped down extension

---
+++ examples/setup.py
@@ -0,0 +1,8 @@
+# Copyright 2023 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+#
+# pylint: disable=missing-module-docstring
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
