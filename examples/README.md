bd8cdd4134dcd414edf978ba4642593938255210
MKLeb <calebb@vmware.com>
2023-05-24 18:01:02 -0400

    Initial stripped down extension

---
+++ examples/README.md
@@ -0,0 +1,5 @@
+# Examples
+
+Example plugins for quick experimentation with Salt Analytics
+
+## Machine Learning: Classify the MNIST handwritten digits
---
+++ examples/src/saltext/examples/__init__.py
@@ -0,0 +1,32 @@
+# Copyright 2023 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+#
+"""
+Define the version.
+"""
+import contextlib
+import pathlib
+
+PACKAGE_ROOT = pathlib.Path(__file__).resolve().parent
+try:
+    from .version import __version__
+except ImportError:  # pragma: no cover
+    __version__ = "0.0.0.not-installed"
+    try:
+        from importlib.metadata import PackageNotFoundError
+        from importlib.metadata import version
+
+        with contextlib.suppress(PackageNotFoundError):
+            __version__ = version(__name__)
+
+    except ImportError:
+        try:
+            from pkg_resources import DistributionNotFound
+            from pkg_resources import get_distribution
+
+            with contextlib.suppress(DistributionNotFound):
+                __version__ = get_distribution(__name__).version
+
+        except ImportError:
+            # pkg resources isn't even available?!
+            pass
