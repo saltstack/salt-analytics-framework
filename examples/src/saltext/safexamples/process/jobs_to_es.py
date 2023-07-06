2a7deb5bfe402af779db6c9dc816466f5c40d9e4
Pedro Algarvio <palgarvio@vmware.com>
2023-07-06 06:30:56 +0100

    Add a `jobs_to_es` processor
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- examples/src/saltext/safexamples/process/jobs_to_es.py
+++ examples/src/saltext/safexamples/process/jobs_to_es.py
@@ -5,46 +5,51 @@ Convert beacon event to an elastic search index event.
 """
 from __future__ import annotations
 
+import json
 import logging
 import pprint
 from typing import TYPE_CHECKING
-from typing import Any
 from typing import AsyncIterator
 from typing import Type
-from typing import cast
 
 from saf.forward.elasticsearch import ElasticSearchEvent
 from saf.models import PipelineRunContext
 from saf.models import ProcessConfigBase
 
 if TYPE_CHECKING:
-    from saf.collect.beacons import BeaconCollectedEvent
+    from saf.process.job_aggregate import JobAggregateCollectedEvent
+
 log = logging.getLogger(__name__)
 
 
-class BeaconToESConfig(ProcessConfigBase):
+class SaltJobsToESConfig(ProcessConfigBase):
     """
     Processor configuration.
     """
 
 
-def get_config_schema() -> Type[BeaconToESConfig]:
+def get_config_schema() -> Type[SaltJobsToESConfig]:
     """
     Get the test collect plugin configuration schema.
     """
-    return BeaconToESConfig
+    return SaltJobsToESConfig
 
 
 async def process(
     *,
-    ctx: PipelineRunContext[BeaconToESConfig],
-    event: BeaconCollectedEvent,
+    ctx: PipelineRunContext[SaltJobsToESConfig],  # noqa: ARG001
+    event: JobAggregateCollectedEvent,
 ) -> AsyncIterator[ElasticSearchEvent]:
     """
     Method called to collect events, in this case, generate.
     """
-    data = cast(dict[str, Any], event.data).copy()
-    data["role"] = ctx.info.salt.role
-    evt = ElasticSearchEvent.construct(index=event.beacon, data=data)
+    data = event.dict()
+    data.pop("data", None)
+    data.update(event.data)
+    # Have the return field always be a JSON string
+    if "return" in data:
+        data["return"] = json.dumps(data["return"])
+    data["@timestamp"] = event.start_time
+    evt = ElasticSearchEvent.construct(index="salt_jobs", data=data)
     log.debug("ElasticSearchEvent: %s", pprint.pformat(evt.dict()))
     yield evt
